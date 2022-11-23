ed4b5e5d9561f392b48ce2a1bf6920f9d00e4938
Кристина <duleba@eml.ru>
2022-11-23 22:31:25 +0700

    устранены замечания

--- src/Employee.java
+++ src/Employee.java
@@ -3,10 +3,9 @@ import java.util.Arrays;
 public class Employee {
     private String fullName;
     // private int [] department = {1, 2, 3, 4, 5};
-    int department;
+    private int department;
     private int salary;
 
-    public static Employee[] employees = new Employee[10];
 
 
     private static int counter;
@@ -16,9 +15,6 @@ public class Employee {
         return this.fullName;
     }
 
-    public int[] getDepartment() {
-        return this.getDepartment();
-    }
 
     public int getSalary() {
         return this.salary;
@@ -45,26 +41,6 @@ public class Employee {
         this.salary = salary;
         id = counter;
         counter++;
-    }
-
-    public static void main(String[] args) {
-        employees[0] = new Employee("Иванов Иван Иванович", 1, 15000);
-        employees[1] = new Employee("Борисов Евгений Петрович", 2, 19000);
-        employees[2] = new Employee("Игнатьева Валентина Семеновна", 3, 18000);
-        employees[3] = new Employee("Романова Александра Сергеевна", 4, 38000);
-        employees[4] = new Employee("Доброгулова Людмила Викторовна", 5, 33000);
-        employees[5] = new Employee("Антонова Татьяна Аркадьевна", 1, 50000);
-        employees[6] = new Employee("Петрова Юлия Сергеевна", 2, 48000);
-        employees[7] = new Employee("Чернов Юрий Юрьевич", 2, 90000);
-        employees[8] = new Employee("Петров Юрий Александрович", 2, 92000);
-        employees[9] = new Employee("Гончарова Анна Дмитриевна", 5, 170000);
-
-        System.out.println(toStringEmploee(employees));
-        System.out.println("Сумма затрат на зарплаты в месяц = " + sum(employees));
-        System.out.println("Сотрудник с максимальной зарплатой : " + max(employees));
-        System.out.println("Сотрудник с минимальной зарплатой : " + min(employees));
-        System.out.println("Среднее значение зарплат :  " + average(employees));
-        fio(employees);
     }
 
     public String toString() {
@@ -108,10 +84,10 @@ public class Employee {
        return employee;
    }
    public static int average (Employee[] employees) {
-        int average = sum(employees) / 10;
+        int average = sum(employees) / employees.length;
        return average;
    }
-   public static void fio (Employee[] employees){
+   public static void printNames (Employee[] employees){
        System.out.println("Все ФИО сотрудников : ");
         for (int i = 0; i< employees.length; i++){
 
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
 public class Main {
     public static void main(String[] args) {
 
+       Employee[] employees = new Employee[10];
+        employees[0] = new Employee("Иванов Иван Иванович", 1, 15000);
+        employees[1] = new Employee("Борисов Евгений Петрович", 2, 19000);
+        employees[2] = new Employee("Игнатьева Валентина Семеновна", 3, 18000);
+        employees[3] = new Employee("Романова Александра Сергеевна", 4, 38000);
+        employees[4] = new Employee("Доброгулова Людмила Викторовна", 5, 33000);
+        employees[5] = new Employee("Антонова Татьяна Аркадьевна", 1, 50000);
+        employees[6] = new Employee("Петрова Юлия Сергеевна", 2, 48000);
+        employees[7] = new Employee("Чернов Юрий Юрьевич", 2, 90000);
+        employees[8] = new Employee("Петров Юрий Александрович", 2, 92000);
+        employees[9] = new Employee("Гончарова Анна Дмитриевна", 5, 170000);
+
+        System.out.println(Employee.toStringEmploee(employees));
+        System.out.println("Сумма затрат на зарплаты в месяц = " + Employee.sum(employees));
+        System.out.println("Сотрудник с максимальной зарплатой : " + Employee.max(employees));
+        System.out.println("Сотрудник с минимальной зарплатой : " + Employee.min(employees));
+        System.out.println("Среднее значение зарплат :  " + Employee.average(employees));
+        Employee.printNames(employees);
 
     }
 }
